d28c3488c32ecc6d21d703cce56b07b5527716e2
Tristan <tristan.gomez@gmail.com>
2015-01-30 13:17:33 +0800

    initial commit

---
+++ lib/github_pivotal_integrations.rb
@@ -0,0 +1,5 @@
+require "github_pivotal_integrations/version"
+
+module GithubPivotalIntegrations
+  # Your code goes here...
+end
---
+++ lib/github_pivotal_integrations/version.rb
@@ -0,0 +1,3 @@
+module GithubPivotalIntegrations
+  VERSION = "0.0.1"
+end
